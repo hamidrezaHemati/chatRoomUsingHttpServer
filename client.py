8a0cfda8dc97ee92b5ea3156b00b10cdd354298a
hamidreza <hrhhemati@gmail.com>
2020-11-14 00:29:31 +0330

    basic post request handler method written in server side and creating a simlpe and basic post request in client that send join to chat room request and user name....also server can generate random token for each client

--- client.py
+++ client.py
@@ -1,6 +1,32 @@
 import http.client as client
+import urllib.parse
 
-connection = client.HTTPConnection('', 9000)
-connection.request("GET", "/")
-response = connection.getresponse()
-print("status: {} and reason: {}".format(response.status, response.reason))
+
+params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
+
+
+def send_user_name(name):
+    name = "hamidreza"
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "name": name}
+    connection = client.HTTPConnection('', 9000)
+    connection.request("POST", "/", params, headers)
+
+
+def join_chat_request(name):
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "name": name, "path": "/join"}
+    connection = client.HTTPConnection('', 9000)
+    connection.request("POST", "/", params, headers)
+    response = connection.getresponse()
+    print("status: {} and reason: {}".format(response.status, response.reason))
+    print(response.headers.auth)
+
+
+def main():
+    name = input("please enter your username: ")
+    join_chat_request(name)
+
+
+
+main()
+#response = connection.getresponse()
+# print("status: {} and reason: {}".format(response.status, response.reason))
--- server.py
+++ server.py
@@ -1,12 +1,35 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import random
+import string
+
+
+def create_auth():
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(10))
+    return result_str
 
 
 class requestHandler(BaseHTTPRequestHandler):
     counter = 0
+
     def do_GET(self):
         self.send_response(200)
-        self.send_header("content-type", 'text/html')
+        self.send_header("content-type", 'text/plain')
         self.end_headers()
+
+    def do_POST(self):
+        if self.headers.get("path") == "/join":
+            print("fuck off")
+            print(self.headers.get("name"))
+            self.send_response(200)
+            auth = create_auth()
+            print("token: ", auth)
+            self.send_header("content-type", 'text/plain')
+            self.send_header("token", auth)
+            self.end_headers()
+        elif self.headers.get("path") == "/message":
+            print("shut the fuck up")
+            print(self.headers.get("name"))
 
 
 def main():
