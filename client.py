a98968ca987f4ce9f2a7baaa818771295c85c03d
hamidreza <hrhhemati@gmail.com>
2020-11-14 20:31:06 +0330

    now server and client can send each type of data to each other using headers...members dictionary emplemented for server and client can get the list of members

--- client.py
+++ client.py
@@ -3,30 +3,63 @@ import urllib.parse
 
 
 params = urllib.parse.urlencode({'@number': 12524, '@type': 'issue', '@action': 'show'})
+auth = ''
 
 
-def send_user_name(name):
-    name = "hamidreza"
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "name": name}
+def get_list_of_users():
     connection = client.HTTPConnection('', 9000)
-    connection.request("POST", "/", params, headers)
+    print("fuck1")
+    connection.request("GET", "/list")
+    response = connection.getresponse()
+    print("status: {} and reason: {}".format(response.status, response.reason))
+    print("members are: ")
+    print(response.headers.get("list"))
+
+
+def send_message(auth):
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "token": auth,
+               "text": "kiramo bokhor"}
+    connection = client.HTTPConnection('', 9000)
+    print("fuck1")
+    connection.request("POST", "/message", params, headers)
 
 
 def join_chat_request(name):
-    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "name": name, "path": "/join"}
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "name": name}
     connection = client.HTTPConnection('', 9000)
-    connection.request("POST", "/", params, headers)
+    connection.request("POST", "/join", params, headers)
     response = connection.getresponse()
-    print("status: {} and reason: {}".format(response.status, response.reason))
-    print(response.headers.auth)
+    auth = response.headers.get("token")
+    print(auth)
+    print(response.status)
+    if response.status == 200:
+        send_message(auth)
+    else:
+        print("status: {} and reason: {}".format(response.status, response.reason))
+    return
 
 
 def main():
-    name = input("please enter your username: ")
-    join_chat_request(name)
+    while 1:
+        code = input("enter code: 1 for joining the chat, 2 for getting the list of active members in group, "
+                     "3 fo sending message and 4 for leave the chat: ")
+        code = int(code)
+        if code == 1:
+            name = input("please enter your username: ")
+            join_chat_request(name)
+        elif code == 2:
+            get_list_of_users()
+        elif code == 3:
+            send_message()
+        elif code == 4:
+            break
+        else:
+            print("wrong code...try again")
 
+    print("you have kicked out of the group chat")
 
 
 main()
-#response = connection.getresponse()
-# print("status: {} and reason: {}".format(response.status, response.reason))
+
+
+#long polling
--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import random
 import string
 
-
+members = {}
 def create_auth():
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(10))
@@ -11,25 +11,26 @@ def create_auth():
 
 class requestHandler(BaseHTTPRequestHandler):
     counter = 0
-
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("content-type", 'text/plain')
-        self.end_headers()
+        if self.path == "/list":
+            print("user wants list")
+            self.send_response(200)
+            self.send_header("content-type", 'text/plain')
+            self.send_header("list", members)
+            self.end_headers()
 
     def do_POST(self):
-        if self.headers.get("path") == "/join":
-            print("fuck off")
-            print(self.headers.get("name"))
-            self.send_response(200)
+        if self.path == "/join":
+            print(self.headers.get("name"), " has joined to the group")
             auth = create_auth()
+            members[self.headers.get("name")] = auth
             print("token: ", auth)
+            self.send_response(200)
             self.send_header("content-type", 'text/plain')
             self.send_header("token", auth)
             self.end_headers()
-        elif self.headers.get("path") == "/message":
-            print("shut the fuck up")
-            print(self.headers.get("name"))
+        elif self.path == "/message":
+            print(self.headers.get("token"), " : ", self.headers.get("text"))
 
 
 def main():
